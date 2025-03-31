fd292cbe816182b69aabce70c0e9444d197e9dc6
VANESTE <florian.vaneste@rossel.fr>
2025-03-31 14:58:28 +0200

    [FIX] Rename package

--- src/RosselKafkaBundle.php
+++ src/RosselKafkaBundle.php
@@ -8,7 +8,7 @@ use Rossel\RosselKafka\Enum\Config\RootConfigKeys;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-final class RosselKafkaPhpKitBundle extends AbstractBundle
+final class RosselKafkaBundle extends AbstractBundle
 {
     private const DEFAULT_PORT = 9092;
 
@@ -17,14 +17,13 @@ final class RosselKafkaPhpKitBundle extends AbstractBundle
         /* @phpstan-ignore method.notFound */
         $definition->rootNode()
             ->children()
-                ->scalarNode(RootConfigKeys::ADDRESS->value)
+                ->scalarNode(RootConfigKeys::HOST->value)
                 ->isRequired()
                 ->cannotBeEmpty()
                 ->info('The IP address or hostname to connect to.')
                 ->end()
             ->integerNode(RootConfigKeys::PORT->value)
                 ->defaultValue(self::DEFAULT_PORT)
-                ->cannotBeEmpty()
                 ->min(1)
                 ->max(65535)
                 ->info(\sprintf('The port number to connect to (default: %s).', self::DEFAULT_PORT))
