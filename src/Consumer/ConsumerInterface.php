14d305c3992121188461a0a452535b451d9ab529
VANESTE <florian.vaneste@rossel.fr>
2025-04-23 11:52:21 +0200

    wip

--- src/Consumer/ConsumerInterface.php
+++ src/Consumer/ConsumerInterface.php
@@ -1,15 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Rossel\RosselKafka\Consumer;
 
 use Rossel\RosselKafka\Enum\Infrastructure\KafkaTopic;
-use Rossel\RosselKafka\Enum\MessageHeaders\MessageType;
+use Rossel\RosselKafka\Model\Message;
 
 interface ConsumerInterface
 {
+    /**
+     * Checks whether the given topic is supported by the consumer.
+     *
+     * @param KafkaTopic $topic the Kafka topic to check
+     *
+     * @return bool true if the topic is supported, false otherwise
+     */
     public function supportsTopic(KafkaTopic $topic): bool;
 
-    public function supportsMessageType(MessageType $messageType): bool;
+    /**
+     * Checks whether the given message type is supported by the consumer.
+     *
+     * @param Message $message the message to check
+     *
+     * @return bool true if the message type is supported, false otherwise
+     */
+    public function supportsMessageType(Message $message): bool;
 
-    public function __invoke();
+    /**
+     * Handles the consumption of a supported message.
+     *
+     * @param Message $message the message to consume
+     *
+     * @return void
+     */
+    public function __invoke(Message $message): void;
 }
--- src/Orchestrator/ConsumptionOrchestrator.php
+++ src/Orchestrator/ConsumptionOrchestrator.php
@@ -6,19 +6,22 @@ namespace Rossel\RosselKafka\Orchestrator;
 
 use Enqueue\RdKafka\RdKafkaMessage;
 use Psr\Log\LoggerInterface;
+use Rossel\RosselKafka\Consumer\ConsumerInterface;
 use Rossel\RosselKafka\Enum\Infrastructure\KafkaTopic;
 use Rossel\RosselKafka\Factory\MessageFactory;
+use Rossel\RosselKafka\Model\Message;
 use Rossel\RosselKafka\Service\Connector\KafkaConnector;
 
-class ConsumptionOrchestrator
+readonly class ConsumptionOrchestrator
 {
-    // private array $consumers = [];
-
+    /**
+     * @param iterable<ConsumerInterface> $consumers
+     */
     public function __construct(
-        private readonly LoggerInterface $logger,
-        // readonly iterable $consumers,
-        private readonly KafkaConnector $kafkaConnector,
-        private readonly MessageFactory $messageFactory,
+        private LoggerInterface $logger,
+        private KafkaConnector $kafkaConnector,
+        private MessageFactory $messageFactory,
+        private iterable $consumers = [],
     ) {
     }
 
@@ -51,8 +54,8 @@ class ConsumptionOrchestrator
                 );
 
                 $rosselMessage = $this->messageFactory->createMessageFromRdKafka($message);
-                dd($rosselMessage);
-                $this->processRosselMessage($rosselMessage);
+
+                $this->processRosselMessage($rosselMessage, $topic);
 
                 // Marque le message comme "traité"
                 $consumer->acknowledge($message);
@@ -60,13 +63,89 @@ class ConsumptionOrchestrator
         }
     }
 
-    private function processRosselMessage(RdKafkaMessage $message): void
-    {
+    private function processRosselMessage(
+        Message $message,
+        KafkaTopic $topic,
+    ): void {
+        $messageId = $message->getRdKafkaMessage()->getMessageId();
+
         $this->logger->debug(
-            'Processing message...',
+            \sprintf('Processing message #%s...', $messageId),
             [
-                'id' => $message->getMessageId(),
+                'id' => $messageId,
             ],
         );
+
+        foreach ($this->consumers as $consumer) {
+            if (true === $this->tryConsumer($consumer, $message, $topic)) {
+                return;
+            }
+        }
+
+        $this->logger->error(
+            \sprintf(
+                'No consumer found for message type %s and topic %s',
+                $message->getType()->name,
+                $topic->name,
+            ),
+            [
+                'id' => $messageId,
+                'topic' => $topic->name,
+                'type' => $message->getType()->name,
+            ]
+        );
+    }
+
+    private function tryConsumer(
+        ConsumerInterface $consumer,
+        Message $message,
+        KafkaTopic $topic,
+    ): bool {
+        $messageId = $message->getRdKafkaMessage()->getMessageId();
+
+        if (($supportsMessageType = $consumer->supportsMessageType($message))
+            && ($supportsTopic = $consumer->supportsTopic($topic))
+        ) {
+            $consumer($message);
+
+            $this->logger->info(
+                \sprintf(
+                    'Message #%s consumed by %s',
+                    $messageId,
+                    $consumer::class,
+                ),
+                [
+                    'id' => $messageId,
+                    'consumer' => $consumer::class,
+                ]
+            );
+
+            return true;
+        }
+
+        $reasons = [];
+
+        if (false === $supportsMessageType) {
+            $reasons['supportsMessageType'] = false;
+        }
+
+        if (isset($supportsTopic) && false === $supportsTopic) {
+            $reasons['supportsTopic'] = false;
+        }
+
+        $this->logger->debug(
+            \sprintf(
+                'Message #%s doesn\'t satisfy the requirements to be consumed by the %s consumer.',
+                $messageId,
+                $consumer::class,
+            ),
+            [
+                'id' => $messageId,
+                'consumer' => $consumer::class,
+                'reasons' => $reasons,
+            ]
+        );
+
+        return false;
     }
 }
